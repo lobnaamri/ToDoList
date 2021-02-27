83bda80ea3306218226e956f0b2e0c49ec9ddbc3
lobna amri <amrilobna1@gmail.com>
2021-02-27 19:57:01 +0100

    Create Todo Model

--- database/migrations/2021_02_27_183742_create_todos_table.php
+++ database/migrations/2021_02_27_183742_create_todos_table.php
@@ -15,6 +15,8 @@ class CreateTodosTable extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('name');
             $table->timestamps();
         });
     }
