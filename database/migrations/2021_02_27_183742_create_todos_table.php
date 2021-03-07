b9104d3dcaf95435f29bd930d0c2b35a86747939
lobna amri <amrilobna1@gmail.com>
2021-03-07 22:28:06 +0100

    update package

--- database/migrations/2021_02_27_183742_create_todos_table.php
+++ database/migrations/2021_02_27_183742_create_todos_table.php
@@ -15,7 +15,8 @@ class CreateTodosTable extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->increments('id', true);
-            $table->integer('user_id')->unsigned()->index();
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('name');
             $table->boolean('completed');
             $table->timestamps();
